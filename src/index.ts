c8459efafcee2144b987fd61bc49acd6fb30eef7
Salim Dellali <dellali.salim@gmail.com>
2025-11-02 11:40:47 -0500

    create TaskManager class skeleton

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,32 @@ interface Task {
   completed: boolean;
 }
 
+class TaskManager {
+  private readonly tasks: Task[] = [];
+
+  addTask(title: string): void {
+    // TODO: implement
+  }
+
+  listTasks(): void {
+    // TODO: implement
+  }
+
+  completeTask(id: string): void {
+    // TODO: implement
+  }
+
+  deleteTask(id: string): void {
+    // TODO: implement
+  }
+
+  saveToFile(): void {
+    // TODO: implement
+  }
+
+  loadFromFile(): void {
+    // TODO: implement
+  }
+}
+
 console.log('Hello World');
